df0031464437a2cfe8af77c19ddc6d2d19945e96
nkapila6 <12816113+nkapila6@users.noreply.github.com>
2024-11-16 19:37:00 +0400

    feat(pipeline-template.py): basic template

---
+++ pipeline-template.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-11-16 19:32:54 Saturday
+
+@author: Nikhil Kapila
+"""
+
+# Basic pipeline
+# TODO: need to add hamilton decorators
+# TODO: need to add mlflow tracking
+
+
+def start():
+    # params to be added:
+    # - which dataset
+    # - which model
+    #   - epochs
+    #   - lr
+    #   - other hyperparams
+    pass
+
+def load_data():
+    pass
+
+def train():
+    pass
+
+def eval_model():
+    pass
+
+def plot():
+    pass
+
+
+def end():
+    pass
