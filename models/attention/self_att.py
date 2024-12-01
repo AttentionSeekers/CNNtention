47e2c1579583701e7d5f32c776ed7a340d5a5e6a
nkapila6 <12816113+nkapila6@users.noreply.github.com>
2024-12-01 10:30:11 +0400

    fix(self_att.py): value mlp called twice

--- models/attention/self_att.py
+++ models/attention/self_att.py
@@ -12,14 +12,14 @@ from torch import nn
 class SelfAtt(nn.Module):
     def __init__(self, in_ch):
         super(SelfAtt, self).__init__()
-        self.weights = nn.Parameter(torch.zeros(1))
+        # self.weights = nn.Parameter(torch.zeros(1))
         self.key = nn.Conv2d(in_ch, in_ch, kernel_size=1)
         self.query = nn.Conv2d(in_ch, in_ch, kernel_size=1)
         self.value = nn.Conv2d(in_ch, in_ch, kernel_size=1)
 
     def forward(self, x): 
         N, ch, h, w = x.shape
-        value = self.value(x).view(N, -1, h*w) # N, ch, h*w
+        # value = self.value(x).view(N, -1, h*w) # N, ch, h*w
         key = self.key(x).view(N, -1, h*w) # N, ch, h*w
         query = self.query(x).view(N, -1, h*w).permute(0, 2 ,1) # N, h*w, ch
 
