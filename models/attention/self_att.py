e2e7ef73f5f1b1746f63f1f2860a152ec13920fe
nkapila6 <12816113+nkapila6@users.noreply.github.com>
2024-12-01 18:37:28 +0400

    feat(self_att.py): added residual connection

--- models/attention/self_att.py
+++ models/attention/self_att.py
@@ -12,10 +12,11 @@ from torch import nn
 class SelfAtt(nn.Module):
     def __init__(self, in_ch):
         super(SelfAtt, self).__init__()
-        # self.weights = nn.Parameter(torch.zeros(1))
+        self.weights = nn.Parameter(torch.zeros(1))
         self.key = nn.Conv2d(in_ch, in_ch, kernel_size=1)
         self.query = nn.Conv2d(in_ch, in_ch, kernel_size=1)
         self.value = nn.Conv2d(in_ch, in_ch, kernel_size=1)
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x, intermediate=False): 
         N, ch, h, w = x.shape
@@ -27,21 +28,24 @@ class SelfAtt(nn.Module):
         # torch.bmm(input, mat2, *, out=None) → Tensor
         # input - bxnxm and mat2 - bxmxp then out is bxnxp
         
-        attention = torch.softmax(
+        attention = self.softmax(
             # query is N, h*w, ch
             # key is N, ch, h*w
             # torch.bmm out -> N, h*w, h*w
             # att = smax(torch.bmm)
-            torch.bmm(query, key),
-            dim=-1
-        )
+            torch.bmm(query, key))
 
         value = self.value(x).view(N, -1, h*w) # N, ch, h*w
 
         out = torch.bmm(value, # N, ch, h*w
-                         attention # N, h*w, h*w
+                         attention.permute(0,2,1) # N, h*w, h*w
                          ).view( # output of bmm is N, ch, h*w
                              N, ch, h, w)
+        
+        # Residual connection
+        # self.weights is a trainable param that further weighs contribution of attention 
+        out = self.weights*out + x
 
+        # return based on inference
         if intermediate: return out, attention, query, key
         else: return out
